13b4884a6d9f1926327ec8df7bf220ae0b7bdbe6
DavidGF <DavidGuFu@gmail.com>
2020-02-10 12:49:22 +0100

    Yii ''ejercicio3''

---
+++ ejemplos/ejemplo3/data/creacion.sql
@@ -0,0 +1,14 @@
+﻿CREATE DATABASE
+  Y2E3;
+USE Y2E3;
+
+CREATE OR REPLACE TABLE catalogo (
+  id int AUTO_INCREMENT,
+  nombre varchar (100),
+  descripcion varchar (100),
+  PRIMARY KEY (id)
+  );
+
+INSERT INTO catalogo VALUES
+  (1,'flor1','Flor del campo'),
+  (2,'flor2','Flor del bosque');
